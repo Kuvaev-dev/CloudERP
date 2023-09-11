1977f2db61a4aacace8d1a8f0401b93d08e98876
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-09-11 21:20:16 +0300

    Login Serring Added

--- CloudERP/Controllers/HomeController.cs
+++ CloudERP/Controllers/HomeController.cs
@@ -24,9 +24,10 @@ namespace CloudERP.Controllers
         [HttpPost]
         public ActionResult LoginUser(string email, string password)
         {
-            var user = db.tblUser.Where(u => u.Email == email && u.Password == password).FirstOrDefault();
+            var user = db.tblUser.Where(u => u.Email == email && u.Password == password && u.IsActive == true).FirstOrDefault();
             if (user != null)
             {
+                Session["UserID"] = user.UserID;
                 Session["UserTypeID"] = user.UserTypeID;
                 Session["FullName"] = user.FullName;
                 Session["Email"] = user.Email;
@@ -34,6 +35,60 @@ namespace CloudERP.Controllers
                 Session["UserName"] = user.UserName;
                 Session["Password"] = user.Password;
                 Session["IsActive"] = user.IsActive;
+
+                var employeeDetails = db.tblEmployee.Where(e => e.UserID == user.UserID).FirstOrDefault();
+                if (employeeDetails == null)
+                {
+                    ViewBag.Message = "Please contact to Administrator";
+
+                    Session["UserTypeID"] = string.Empty;
+                    Session["FullName"] = string.Empty;
+                    Session["Email"] = string.Empty;
+                    Session["ContactNo"] = string.Empty;
+                    Session["UserName"] = string.Empty;
+                    Session["Password"] = string.Empty;
+                    Session["IsActive"] = string.Empty;
+                    Session["EmployeeID"] = string.Empty;
+                    Session["EName"] = string.Empty;
+                    Session["EPhoto"] = string.Empty;
+                    Session["Designation"] = string.Empty;
+                    Session["BranchID"] = string.Empty;
+                    Session["CompanyID"] = string.Empty;
+
+                    return View("Login");
+                }
+
+                Session["EmployeeID"] = employeeDetails.EmployeeID;
+                Session["EName"] = employeeDetails.Name;
+                Session["EPhoto"] = employeeDetails.Photo;
+                Session["Designation"] = employeeDetails.Designation;
+                Session["BranchID"] = employeeDetails.BranchID;
+                Session["CompanyID"] = employeeDetails.CompanyID;
+
+                var company = db.tblCompany.Where(c => c.CompanyID == employeeDetails.CompanyID).FirstOrDefault();
+                if (company == null)
+                {
+                    ViewBag.Message = "Please contact to Administrator";
+
+                    Session["UserTypeID"] = string.Empty;
+                    Session["FullName"] = string.Empty;
+                    Session["Email"] = string.Empty;
+                    Session["ContactNo"] = string.Empty;
+                    Session["UserName"] = string.Empty;
+                    Session["Password"] = string.Empty;
+                    Session["IsActive"] = string.Empty;
+                    Session["EmployeeID"] = string.Empty;
+                    Session["EName"] = string.Empty;
+                    Session["EPhoto"] = string.Empty;
+                    Session["Designation"] = string.Empty;
+                    Session["BranchID"] = string.Empty;
+                    Session["CompanyID"] = string.Empty;
+
+                    return View("Login");
+                }
+
+                Session["CName"] = company.Name;
+                Session["CLogo"] = company.Logo;
 
                 return RedirectToAction("Index");
             }
@@ -48,6 +103,12 @@ namespace CloudERP.Controllers
                 Session["UserName"] = string.Empty;
                 Session["Password"] = string.Empty;
                 Session["IsActive"] = string.Empty;
+                Session["EmployeeID"] = string.Empty;
+                Session["EName"] = string.Empty;
+                Session["EPhoto"] = string.Empty;
+                Session["Designation"] = string.Empty;
+                Session["BranchID"] = string.Empty;
+                Session["CompanyID"] = string.Empty;
             }
 
             return View("Login");
