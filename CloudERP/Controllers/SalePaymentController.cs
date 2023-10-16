35db6fca811b8c6b8e68ad0a06cb5ca5a3d02488
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-10-16 18:00:29 +0300

    Sale Logic Added

--- CloudERP/Controllers/SalePaymentController.cs
+++ CloudERP/Controllers/SalePaymentController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DatabaseAccess.Code.SP_Code;
+using DatabaseAccess.Code;
+using DatabaseAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,128 @@ namespace CloudERP.Controllers
 {
     public class SalePaymentController : Controller
     {
-        // GET: SalePayment
+        private CloudDBEntities db = new CloudDBEntities();
+        private SP_Sale sale = new SP_Sale();
+        private SaleEntry saleEntry = new SaleEntry();
+
+        // GET: PurchasePayment
         public ActionResult RemainingPaymentList()
         {
-            return View();
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var list = sale.RemainingPaymentList(companyID, branchID);
+            return View(list.ToList());
+        }
+
+        public ActionResult PaidHistory(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])) || string.IsNullOrEmpty(Convert.ToString(id)))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var list = sale.SalePaymentHistory((int)id);
+            return View(list.ToList());
+        }
+
+        public ActionResult PaidAmount(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])) || string.IsNullOrEmpty(Convert.ToString(id)))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var list = sale.SalePaymentHistory((int)id);
+            double remainingAmount = 0;
+            foreach (var item in list)
+            {
+                remainingAmount = item.RemainingBalance;
+            }
+            if (remainingAmount == 0)
+            {
+                remainingAmount = db.tblCustomerInvoice.Find(id).TotalAmount;
+            }
+            ViewBag.PreviousRemainingAmount = remainingAmount;
+            ViewBag.InvoiceID = id;
+            return View(list.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult PaidAmount(int? id, float previousRemainingAmount, float paymentAmount)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])) || string.IsNullOrEmpty(Convert.ToString(id)))
+                {
+                    return RedirectToAction("Login", "Home");
+                }
+                int companyID = 0;
+                int branchID = 0;
+                int userID = 0;
+                branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+                companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+                userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+                if (paymentAmount > previousRemainingAmount)
+                {
+                    ViewBag.Message = "Payment Must be Less Then or Equal to Previous Remaining Amount!";
+                    var list = sale.SalePaymentHistory((int)id);
+                    double remainingAmount = 0;
+                    foreach (var item in list)
+                    {
+                        remainingAmount = item.RemainingBalance;
+                    }
+                    if (remainingAmount == 0)
+                    {
+                        remainingAmount = db.tblCustomerInvoice.Find(id).TotalAmount;
+                    }
+                    ViewBag.PreviousRemainingAmount = remainingAmount;
+                    ViewBag.InvoiceID = id;
+                    return View(list);
+                }
+                string payinvoicenno = "INP" + DateTime.Now.ToString("yyyyMMddHHmmss") + DateTime.Now.Millisecond;
+                var customer = db.tblCustomer.Find(db.tblCustomerInvoice.Find(id).CustomerID);
+                var purchaseInvoice = db.tblSupplierInvoice.Find(id);
+                var purchasePaymentDetails = db.tblSupplierPayment.Where(p => p.SupplierInvoiceID == id);
+                string message = saleEntry.SalePayment(companyID, branchID, userID, payinvoicenno, Convert.ToString(id), (float)purchaseInvoice.TotalAmount,
+                    paymentAmount, Convert.ToString(customer.CustomerID), customer.Customername, previousRemainingAmount - paymentAmount);
+                Session["Message"] = message;
+                return RedirectToAction("RemainingPaymentList");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Please Try Again!";
+                var list = sale.SalePaymentHistory((int)id);
+                double remainingAmount = 0;
+                foreach (var item in list)
+                {
+                    remainingAmount = item.RemainingBalance;
+                }
+                if (remainingAmount == 0)
+                {
+                    remainingAmount = db.tblCustomerInvoice.Find(id).TotalAmount;
+                }
+                ViewBag.PreviousRemainingAmount = remainingAmount;
+                ViewBag.InvoiceID = id;
+                return View(list);
+            }
         }
     }
 }
---
+++ DatabaseAccess/Code/SP_Code/SP_Sale.cs
@@ -0,0 +1,101 @@
+﻿using DatabaseAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess.Code.SP_Code
+{
+    public class SP_Sale
+    {
+        private CloudDBEntities db = new CloudDBEntities();
+
+        public List<SalePaymentModel> RemainingPaymentList(int CompanyID, int BranchID)
+        {
+            var remainingPaymentList = new List<SalePaymentModel>();
+            SqlCommand command = new SqlCommand("GetCustomerRemainingPaymentRecord", DatabaseQuery.ConnOpen())
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.AddWithValue("@BranchID", BranchID);
+            command.Parameters.AddWithValue("@CompanyID", CompanyID);
+            var dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
+            {
+                var customerID = Convert.ToInt32(Convert.ToString(row[4]));
+                var customer = db.tblCustomer.Find(customerID);
+                var payment = new SalePaymentModel();
+                payment.CustomerInvoiceID = Convert.ToInt32(Convert.ToString(row[0]));
+                payment.BranchID = Convert.ToInt32(Convert.ToString(row[1]));
+                payment.CompanyID = Convert.ToInt32(Convert.ToString(row[2]));
+                payment.InvoiceDate = Convert.ToDateTime(Convert.ToString(row[3]));
+                payment.InvoiceNo = Convert.ToString(row[5]);
+                double payAmount = 0;
+                double.TryParse(Convert.ToString(row[7]), out payAmount);
+                double remainingBalance = 0;
+                double.TryParse(Convert.ToString(row[8]), out remainingBalance);
+                double totalAmount = 0;
+                double.TryParse(Convert.ToString(row[6]), out totalAmount);
+                payment.PaymentAmount = payAmount;
+                payment.RemainingBalance = remainingBalance;
+                payment.CustomerContactNo = customer.CustomerContact;
+                payment.CustomerAddress = customer.CustomerAddress;
+                payment.CustomerID = customer.CustomerID;
+                payment.CustomerName = customer.Customername;
+                payment.TotalAmount = totalAmount;
+
+                remainingPaymentList.Add(payment);
+            }
+            return remainingPaymentList;
+        }
+
+        public List<SalePaymentModel> SalePaymentHistory(int CustomerInvoiceID)
+        {
+            var remainingPaymentList = new List<SalePaymentModel>();
+            SqlCommand command = new SqlCommand("GetCustomerPaymentHistory", DatabaseQuery.ConnOpen())
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.AddWithValue("@CustomerInvoiceID", CustomerInvoiceID);
+            var dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
+            {
+                var customerID = Convert.ToInt32(Convert.ToString(row[4]));
+                var userID = Convert.ToInt32(Convert.ToString(row[9]));
+                var customer = db.tblCustomer.Find(customerID);
+                var user = db.tblUser.Find(userID);
+                var payment = new SalePaymentModel();
+                payment.CustomerInvoiceID = Convert.ToInt32(Convert.ToString(row[0]));
+                payment.BranchID = Convert.ToInt32(Convert.ToString(row[1]));
+                payment.CompanyID = Convert.ToInt32(Convert.ToString(row[2]));
+                payment.InvoiceDate = Convert.ToDateTime(Convert.ToString(row[3]));
+                payment.InvoiceNo = Convert.ToString(row[5]);
+                double payAmount = 0;
+                double.TryParse(Convert.ToString(row[7]), out payAmount);
+                double remainingBalance = 0;
+                double.TryParse(Convert.ToString(row[8]), out remainingBalance);
+                double totalAmount = 0;
+                double.TryParse(Convert.ToString(row[6]), out totalAmount);
+                payment.PaymentAmount = payAmount;
+                payment.RemainingBalance = remainingBalance;
+                payment.CustomerContactNo = customer.CustomerContact;
+                payment.CustomerAddress = customer.CustomerAddress;
+                payment.CustomerID = customer.CustomerID;
+                payment.CustomerName = customer.Customername;
+                payment.TotalAmount = totalAmount;
+                payment.UserID = user.UserID;
+                payment.UserName = user.UserName;
+
+                remainingPaymentList.Add(payment);
+            }
+            return remainingPaymentList;
+        }
+    }
+}
