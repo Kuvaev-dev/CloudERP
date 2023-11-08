22f77ebec368b25a0b268f21b82cee4d651f3b0b
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-11-08 21:11:50 +0200

    Employee Salary Logic Added

--- CloudERP/Controllers/CompanyEmployeeController.cs
+++ CloudERP/Controllers/CompanyEmployeeController.cs
@@ -1,5 +1,7 @@
 ﻿using CloudERP.Helpers;
+using CloudERP.Models;
 using DatabaseAccess;
+using DatabaseAccess.Code;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -12,6 +14,7 @@ namespace CloudERP.Controllers
     public class CompanyEmployeeController : Controller
     {
         private CloudDBEntities db = new CloudDBEntities();
+        private SalaryTransaction salaryTransaction = new SalaryTransaction();
 
         // GET: Employees
         public ActionResult Employees()
@@ -76,6 +79,87 @@ namespace CloudERP.Controllers
             }
 
             return View(employee);
+        }
+
+        public ActionResult GetEmployee(string cnic) // CNIC
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var employee = db.tblEmployee.Where(p => p.CNIC == cnic).FirstOrDefault();
+            return Json(new { data = employee }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult EmployeeSalary()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+            var salary = new SalaryMV();
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            salary.SalaryMonth = DateTime.Now.AddMonths(-1).ToString("MMMM");
+            salary.SalaryYear = DateTime.Now.AddMonths(-1).ToString("yyyy");
+            return View(salary);
+        }
+
+        [HttpPost]
+        public ActionResult EmployeeSalary(SalaryMV salary)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])))
+                {
+                    return RedirectToAction("Login", "Home");
+                }
+                int companyID = 0;
+                int branchID = 0;
+                int userID = 0;
+                branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+                companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+                userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+                salary.SalaryMonth = salary.SalaryMonth.ToLower();
+                var emp = db.tblPayroll.Where(p => p.EmployeeID == salary.EmployeeID && p.BranchID==branchID && p.CompanyID==companyID && p.SalaryMonth == salary.SalaryMonth && p.SalaryYear == salary.SalaryYear).FirstOrDefault();
+                if (emp != null)
+                {
+                    string invoiceNo = "ESA" + DateTime.Now.ToString("yyyyMMddHHmmss") + DateTime.Now.Millisecond;
+                    string message = string.Empty;
+                    if (ModelState.IsValid)
+                    {
+                        message = salaryTransaction.Confirm(salary.EmployeeID, salary.TransferAmount, userID, branchID, companyID, invoiceNo, salary.SalaryMonth, salary.SalaryYear);
+                    }
+                    if (message.Contains("Succeed"))
+                    {
+                        Session["SalaryMessage"] = message;
+                    }
+                    else
+                    {
+                        Session["SalaryMessage"] = "Salary is Already Paid";
+                    }
+                }
+                else
+                {
+                    Session["SalaryMessage"] = "Please Re-Login and Try Again";
+                }
+                return View(salary);
+            }
+            catch
+            {
+                Session["SalaryMessage"] = "Some Unexpected Issue is Occure. Please Try Again";
+                return View(salary);
+            }
         }
     }
 }
---
+++ CloudERP/Models/SalaryMV.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CloudERP.Models
+{
+    public class SalaryMV
+    {
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "*Required!")]
+        [DataType(DataType.Currency)]
+        public double TransferAmount { get; set; }
+        [Required(ErrorMessage = "*Required!")]
+        public string SalaryMonth { get; set; }
+        [Required(ErrorMessage = "*Required!")]
+        public string SalaryYear { get; set; }
+    }
+}
