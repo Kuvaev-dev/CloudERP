a8516ab65414d0dff108859f7eacb3efe48d77d3
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2023-11-12 16:56:11 +0200

    Trial Balance Added

---
+++ CloudERP/Controllers/TrialBalanceController.cs
@@ -0,0 +1,79 @@
+﻿using DatabaseAccess;
+using DatabaseAccess.Code;
+using DatabaseAccess.Code.SP_Code;
+using DatabaseAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudERP.Controllers
+{
+    public class TrialBalanceController : Controller
+    {
+        CloudDBEntities db = new CloudDBEntities();
+        SP_TrialBalance trialBalance = new SP_TrialBalance();
+
+        // GET: TrialBalance
+        public ActionResult GetTrialBalance()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var financialYearCheck = DatabaseQuery.Retrive("select top 1 FinancialYearID from tblFinancialYear where IsActive = 1");
+            string FinancialYearID = (financialYearCheck != null ? Convert.ToString(financialYearCheck.Rows[0][0]) : string.Empty);
+            List<TrialBalanceModel> list = new List<TrialBalanceModel>();
+            if (string.IsNullOrEmpty(FinancialYearID))
+            {
+                list = trialBalance.TriaBalance(branchID, companyID, 0);
+            }
+            else
+            {
+                list = trialBalance.TriaBalance(branchID, companyID, Convert.ToInt32(FinancialYearID));
+            }
+            return View(list);
+        }
+
+        [HttpPost]
+        public ActionResult GetTrialBalance(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["CompanyID"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int companyID = 0;
+            int branchID = 0;
+            int userID = 0;
+            branchID = Convert.ToInt32(Convert.ToString(Session["BranchID"]));
+            companyID = Convert.ToInt32(Convert.ToString(Session["CompanyID"]));
+            userID = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            var financialYearCheck = DatabaseQuery.Retrive("select top 1 FinancialYearID from tblFinancialYear where IsActive = 1");
+            string FinancialYearID = (financialYearCheck != null ? Convert.ToString(financialYearCheck.Rows[0][0]) : string.Empty);
+            List<TrialBalanceModel> list = new List<TrialBalanceModel>();
+
+            list = trialBalance.TriaBalance(branchID, companyID, (int)id);
+
+            return View(list);
+        }
+
+        public ActionResult GetFinancialYear()
+        {
+            var getList = db.tblFinancialYear.ToList();
+            var list = new List<tblFinancialYear>();
+            foreach (var item in getList)
+            {
+                list.Add(new tblFinancialYear() { FinancialYearID = item.FinancialYearID, FinancialYear = item.FinancialYear });
+            }
+
+            return Json(new { data = list }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DatabaseAccess/Code/SP_Code/SP_TrialBalance.cs
@@ -0,0 +1,54 @@
+﻿using DatabaseAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess.Code.SP_Code
+{
+    public class SP_TrialBalance
+    {
+        public List<TrialBalanceModel> TriaBalance(int BranchID, int CompanyID, int FinancialYearID)
+        {
+            var triaBalance = new List<TrialBalanceModel>();
+            SqlCommand command = new SqlCommand("GetTrialBalance", DatabaseQuery.ConnOpen())
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.AddWithValue("@BranchID", BranchID);
+            command.Parameters.AddWithValue("@CompanyID", CompanyID);
+            command.Parameters.AddWithValue("@FinancialYearID", FinancialYearID);
+            var dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dt);
+            double totalDebit = 0;
+            double totalCredit = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                var balance = new TrialBalanceModel();
+                balance.FinancialYearID = Convert.ToInt32(Convert.ToString(row[0]));
+                balance.AccountSubControl = Convert.ToString(row[1]);
+                balance.AccountSubControlID = Convert.ToInt32(row[2]);
+                balance.Debit = Convert.ToDouble(row[3] == DBNull.Value ? 0 : row[3]);
+                balance.Credit = Convert.ToDouble(row[4] == DBNull.Value ? 0 : row[4]);
+                balance.BranchID = Convert.ToInt32(row[5]);
+                balance.CompanyID = Convert.ToInt32(row[6]);
+                totalDebit += Convert.ToDouble(row[3] == DBNull.Value ? 0 : row[3]);
+                totalCredit += Convert.ToDouble(row[4] == DBNull.Value ? 0 : row[4]);
+                if (balance.Debit > 0 || balance.Credit > 0)
+                {
+                    triaBalance.Add(balance);
+                }
+            }
+            var totalBalance = new TrialBalanceModel();
+            totalBalance.Credit = totalCredit;
+            totalBalance.Debit = totalDebit;
+            totalBalance.AccountSubControl = "Total";
+            triaBalance.Add(totalBalance);
+            return triaBalance;
+        }
+    }
+}
